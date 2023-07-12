b1aa26bb2f0b847d3628e3a8968ae2ad8eaedb80
san-dya12 <139223675+san-dya12@users.noreply.github.com>
2023-07-12 22:46:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,3 +68,4 @@ try:
      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
      back_from_function = insert_row_snowflake(add_my_fruit)
      streamlit.text(back_from_function)
+    
