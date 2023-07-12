2a0fbb27e970ed7505908d303fee2fc89cb15716
san-dya12 <139223675+san-dya12@users.noreply.github.com>
2023-07-12 20:06:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,5 +52,10 @@ my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
+
+my_data_row = my_cur.fetchone()
+streamlit.header("The fruits load list contains:")
+streamlit.dataframe(my_data_row)
+
   
 
